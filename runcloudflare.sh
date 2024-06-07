b795ebe4ffe6773c355ad22a902253089e4ba6b1
ph502 <43583290+ph502@users.noreply.github.com>
2024-06-07 10:53:58 -0600

    runcloudflare.sh

--- runcloudflare.sh
+++ runcloudflare.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/data/data/com.termux/files/home/go/bin/httpx -l cloudflare.txt -status-code -match-string cloudflare -p 80,443 -threads 1700  -o cflaresults.txt
+httpx -l cloudflare.txt -status-code -match-string cloudflare -p 80,443 -threads 1700  -o cflaresults.txt
