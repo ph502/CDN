c8a2ea8506c99b6a21991f046889540fd4e68187
ph502 <43583290+ph502@users.noreply.github.com>
2024-06-07 10:53:32 -0600

    runbunny.sh

--- runbunny.sh
+++ runbunny.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/data/data/com.termux/files/home/go/bin/httpx -l bunny.txt -mc 200 -server -p 80,443 -t 1000 -o bunnyresults.txt
+httpx -l bunny.txt -mc 200 -server -p 80,443 -t 1000 -o bunnyresults.txt
