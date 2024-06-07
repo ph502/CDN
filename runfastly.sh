229a983d2460b381232b7426f69e3be75b046f86
ph502 <43583290+ph502@users.noreply.github.com>
2024-06-07 10:22:40 -0600

    runfastly.sh

--- runfastly.sh
+++ runfastly.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-httpx -l fastly.txt -status-code -match-string cloudfront -p 80,443 -threads 1700  -o fastlyresults.txt
+/data/data/com.termux/files/home/go/bin/httpx -l fastly.txt -status-code -match-string cloudfront -p 80,443 -threads 1700  -o fastlyresults.txt
