00a085c5166f294bd2d4492b3639297e61b9b75c
ph502 <43583290+ph502@users.noreply.github.com>
2024-06-07 10:54:27 -0600

    runcloudfront.sh

--- runcloudfront.sh
+++ runcloudfront.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/data/data/com.termux/files/home/go/bin/httpx -l cloudfront.txt -status-code -match-string cloudfront -p 80,443 -threads 1700  -o cfrontresults.txt
+httpx -l cloudfront.txt -status-code -match-string cloudfront -p 80,443 -threads 1700  -o cfrontresults.txt
