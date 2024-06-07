32791144a57ea81daee65d2b90f18d9e647507c9
ph502 <43583290+ph502@users.noreply.github.com>
2024-06-07 10:22:12 -0600

    runedgio.sh

--- runedgio.sh
+++ runedgio.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-httpx -l edgio.txt -status-code -match-string 404 -p 80,443 -threads 1700  -o edgioresults.txt
+/data/data/com.termux/files/home/go/bin/httpx -l edgio.txt -status-code -match-string 404 -p 80,443 -threads 1700  -o edgioresults.txt
