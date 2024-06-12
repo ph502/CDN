ccba7430f0324990d4f29ed8920c24f83b70ed36
ph502 <43583290+ph502@users.noreply.github.com>
2024-06-12 16:26:12 -0600

    runcloudflare.sh
    output reading line was corrected

--- runcloudflare.sh
+++ runcloudflare.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-httpx -l cloudflare.txt -status-code -match-string cloudflare -p 80,443 -threads 1700  -o cflaresults.txt
+httpx -l cloudflare.txt -status-code -match-string cloudflare -p 80,443 -threads 1700 -o cflaresults.txt
