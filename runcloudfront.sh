9d3cfa31e42a76f1e4c70c64c5566562f6a13d77
ph502 <43583290+ph502@users.noreply.github.com>
2024-06-24 12:27:03 -0600

    runcloudfront.sh

--- runcloudfront.sh
+++ runcloudfront.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-httpx -l cloudfront.txt -status-code -match-string cloudfront -p 80,443 -threads 1700  -o cfrontresults.txt
+httpx -l cloudfront.txt -sc -ms cloudfront -p 80,443 -t 1700 -o resucfront.txt
