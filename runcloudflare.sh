b05c4e2c3d876a6b9cdb940fc84abb68dfdedb36
ph502 <43583290+ph502@users.noreply.github.com>
2024-06-24 12:25:52 -0600

    runcloudflare.sh

--- runcloudflare.sh
+++ runcloudflare.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-httpx -l cloudflare.txt -status-code -match-string cloudflare -p 80,443 -threads 1700 -o cflaresults.txt
+httpx -l cloudflare.txt -sc -ms cloudflare -p 80,443 -t 1700 -o resclfare.txt
