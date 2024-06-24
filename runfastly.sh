0e6b0e52cf94d52dece769bdb6e4174054725214
ph502 <43583290+ph502@users.noreply.github.com>
2024-06-24 12:37:24 -0600

    runfastly.sh

--- runfastly.sh
+++ runfastly.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-httpx -l fastly.txt -status-code -match-string fastly -p 80,443 -threads 1700  -o fastlyresults.txt
+httpx -l fastly.txt -sc -ms fastly -p 80,443 -t 1700 -o resufastly.txt
